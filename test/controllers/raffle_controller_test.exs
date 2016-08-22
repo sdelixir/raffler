bd1806a3e8ac284bde02ec8ad62a5e348abfdfee
Kevin Sullivan <kevinmsullivan@protonmail.com>
2016-08-22 08:32:12 -0700

    add session controller test, add raffle controller test

--- test/controllers/raffle_controller_test.exs
+++ test/controllers/raffle_controller_test.exs
@@ -7,27 +7,30 @@ defmodule Raffler.RaffleControllerTest do
 
   test "GET /raffles", %{conn: conn} do
     conn = get conn, "/raffles"
+    
     assert html_response(conn, 200) =~ "Past Raffles"
   end
 
   test "GET /raffles/new", %{conn: conn} do
     conn = get conn, "/raffles/new"
+
     assert html_response(conn, 200) =~ "New Raffle"
   end
 
   test "GET /raffles/show", %{conn: conn} do
     raffle = insert_raffle()
     conn = get conn, "/raffles/#{raffle.id}"
+
     assert html_response(conn, 200) =~ "(#{raffle.id}) - Raffle - #{raffle.date}"
   end
 
-  test "POST /raffle - creates raffle and redirects", %{conn: conn} do
+  test "POST /raffles - creates raffle and redirects when valid", %{conn: conn} do
     conn = post conn, raffle_path(conn, :create), raffle: @valid_attrs
 
     assert redirected_to(conn) == raffle_path(conn, :index)
   end
 
-  test "does not create video and renders error when invalid", %{conn: conn} do
+  test "POST /raffles - does not create raffle and renders error when invalid", %{conn: conn} do
     count_before = raffle_count(Raffle)
     conn = post conn, raffle_path(conn, :create), raffle: @invalid_attrs
 
