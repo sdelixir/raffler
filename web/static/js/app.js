94324c4bcc1601b17e891ad5df76620fd85c3790
Kevin Sullivan <kevinmsullivan@protonmail.com>
2016-08-27 20:27:15 -0700

    Add dice image, css, js

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,30 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+// DICE JS
+var dice;
+var dices = ['&#9856;', '&#9857;', '&#9858;', '&#9859;', '&#9860;', '&#9861;' ];
+var stopped = true;
+var t;
+
+function change() {
+  var random = Math.floor(Math.random()*6);
+  dice.innerHTML = dices[random];
+}
+
+function stopstart() {
+  if(stopped) {
+    stopped = false;
+    t = setInterval(change, 500);
+  } else {
+    clearInterval(t);
+    stopped = true;
+  }
+
+}
+
+window.onload = function() {
+  dice = document.getElementById("dice");
+  stopstart();
+}
